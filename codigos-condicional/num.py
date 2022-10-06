e87b113a327d7ffce0c2651f09979905c61994db
fernandamelreis <fernanda.melreis@gmail.com>
2022-10-06 15:57:07 -0300

    Codes

---
+++ codigos-condicional/num.py
@@ -0,0 +1,9 @@
+num1 = int(input("Digite o 1o. numero_"))
+
+num2 = int(input("Digite o 2o. numero_"))
+
+
+if num1 > num2:
+    print("O primeiro numero eh o maior!")
+else:
+    print("O segundo numero eh o maior!")
---
+++ codigos-condicional/parimpar.py
@@ -0,0 +1,7 @@
+num = int(input("Digite o numero: "))
+
+
+if num%2 == 0:
+    print("O numero eh par!")
+else:
+    print("O numero eh impar!")
