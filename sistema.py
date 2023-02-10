07949e647d11b2abfa28d8b79ca7af9fd919191e
Fernanda de Melo Reis <114718084+fernandamelreis@users.noreply.github.com>
2023-02-10 16:50:51 -0300

    Exemplo com elif
    Código com as estruturas condicionais if, elif e else simulando um sistema de cadastro e autenticação do usuário.

---
+++ sistema.py
@@ -0,0 +1,48 @@
+print("[1] CADASTRO")
+print("[2] AUTENTICAÇÃO")
+print("[3] SAIR")
+
+
+opcao = int(input("Digite uma opção: "))
+
+if opcao == 1:
+    print("---=REGISTER ----")
+
+    nome = input("Digite o seu nome completo: ") #string
+    idade = int(input("Digite a sua idade: "))
+    email = input("Digite o seu e-mail: ")
+    senha = input("Digite a sua senha: ")
+    cpf = input("Digite o seu CPF: ")
+
+
+    print("Nome: %s" %nome)
+    print("Idade: %d" %idade)
+    print("E-mail: %s" %email)
+    print("Senha: %s" %senha)
+    print("CPF: %s" %cpf)
+    print("Successfully registered user!")
+
+elif opcao == 2:
+    
+    print("---AUTENTICATION---")
+    login = input("Login: ")
+    password = input("Password: ")
+
+    print("Login: %s" %login)
+    print("Password: ********" )
+    print("Successyfully authenticated user!")
+
+elif opcao ==3:
+    
+    print("LOGOFF...")
+    print("Responda Y para SIM e N para NÃO")
+    resp = input("Deseja realmente sair do sistema? ___")
+
+    if resp == "Y":
+        print("Usuário está sendo deslogado do sistema...")
+    else:
+        print("Usuário continua conectado no sistema...")
+
+else:
+    print("Opção inválida escolha 1, 2 ou 3!")
+
